e4d2e9ebac46b221c2df0920b9ca3039e4a9f8e9
diasds.marcos <diasds.marcos@gmail.com>
2021-12-21 19:06:49 -0300

    criado tela de listagem de eventos

--- agenda/urls.py
+++ agenda/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from os import uname_result
+
 from django.contrib import admin
 from django.urls import path
+from core import views
+from django.views.generic import  RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('agenda/', views.lista_eventos),
+    path('',RedirectView.as_view(url='/agenda/'))
+    #path('',views.index)
+
 ]
